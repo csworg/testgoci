65e748a296f9869dcc3e5c974277eebd0abda6a5
cwjuggernaut <chris.walker@netfoundry.io>
2021-09-08 12:33:46 -0400

    executing workflow w/vulnerability: 2021-09-07-12:44:41

--- main.go
+++ main.go
@@ -1,33 +1,54 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"text/template"
 )
 
-func mp() {
-	m := make(map[string]string)
-	m["a"] = "alpha"
-	m["b"] = "beta"
+type TodoPageData struct {
+	PageTitle string
+	Todos []Todo
 
-	for key, value := range m {
-	fmt.Println("key", key, "value", value)
-	}
 }
 
-func array() {
-	arr := []string{"one", "two", "three"}
-	for key, value := range arr {
-		fmt.Println("index", key, "value", value)
-	}
+type Todo struct {
+	Title string "json:title"
+	Done bool "json:done"
 }
 
-func sum(a int, b int) (int) {
-	sum := a+b;
-	return sum;
+func (t Todo) ToString() string {
+	bytes, _ := json.Marshal(t)
+	return string(bytes)
 }
 
+func getTodos() []Todo {
+	todos := make([]Todo, 3)
+	raw, _ := ioutil.ReadFile("./todos.json")
+	json.Unmarshal(raw, &todos)
+	return todos
+
+}
 
 func main() {
-	sum := sum(2, 2)
-	fmt.Println("\nRESULT => Sum is =>", sum)
+	//tmpl := template.Must(template.ParseFiles("template/index.html"))
+	//tmpl := template.Must(template.ParseFiles("/Users/victoria/go/src/cswgo/template/index.html"))
+	tmpl, err := template.ParseFiles("/Users/victoria/go/src/cswgo/template/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData {
+			PageTitle: "My Todos!",
+			Todos: getTodos(),
+		}
+
+		tmpl.Execute(w, data)
+
+	})
+
+	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
 }
