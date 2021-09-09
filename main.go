50e455c7eac85b77aedd18f2134c1aad0b0b07c6
cwjuggernaut <chris.walker@netfoundry.io>
2021-09-09 10:52:15 -0400

    executing workflow w/vulnerability: 2021-09-07-12:44:41

--- main.go
+++ main.go
@@ -1,52 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	"fmt"
 	"net/http"
 	"os"
-	"text/template"
 )
 
-type TodoPageData struct {
-	PageTitle string
-	Todos []Todo
-
-}
-
-type Todo struct {
-	Title string "json:title"
-	Done bool "json:done"
-}
-
-func (t Todo) ToString() string {
-	bytes, _ := json.Marshal(t)
-	return string(bytes)
-}
-
-func getTodos() []Todo {
-	todos := make([]Todo, 3)
-	raw, _ := ioutil.ReadFile("./todos.json")
-	json.Unmarshal(raw, &todos)
-	return todos
-
-}
-
+//CSW Simple HTTP Server to test GoSec
 func main() {
-	//tmpl := template.Must(template.ParseFiles("/Users/victoria/go/src/cswgo/template/index.html"))
-	tmpl, err := template.ParseFiles("/Users/victoria/go/src/cswgo/template/index.html")
-	if err != nil {
-		panic(err)
-	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := TodoPageData {
-			PageTitle: "My Todos!",
-			Todos: getTodos(),
-		}
-
-		tmpl.Execute(w, data)
-
+		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	})
 
 	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
