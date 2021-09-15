fbe973fef9f1cc7c66eb1a2d13cb9c3181d036fc
cwjuggernaut <chris.walker@netfoundry.io>
2021-09-15 13:46:59 -0400

    executing workflow w/vulnerability: 2021-09-07-12:44:41

--- main.go
+++ main.go
@@ -4,13 +4,31 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"text/template"
 )
+
+type PageData struct {
+	name string
+}
 
 //CSW Simple HTTP Server to test GoSec
 func main() {
 
+	tmpl := template.Must(template.ParseFiles("template/index.html"))
+	websitename := "cw-awesome-website"
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+		data := PageData {
+			name: websitename,
+		}
+
+		if r.URL.Path[1:] != "" {
+			fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+		} else {
+			tmpl.Execute(w, data)
+			//fmt.Fprintf(w, "Hi there, you have reached Chris simple Go web server")
+		}
 	})
 
 	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
